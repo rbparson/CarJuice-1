83036606ee224b84e5c5e941fd08238df058d8b4
Jason Brown <phishy.signups@gmail.com>
2012-05-06 21:59:07 -0400

    JSON parser working on test String, fully adding all fields to StationInfo

--- src/ncsu/carjuice/main/CarJuiceActivity.java
+++ src/ncsu/carjuice/main/CarJuiceActivity.java
@@ -15,29 +15,42 @@ public class CarJuiceActivity extends Activity {
 	//public final static String EXTRA_MESSAGE = "ncsu.carjuice.main.MESSAGE";
 
     /** Called when the activity is first created. */
+   	
     @Override
+    //@param savedInstanceState
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
+/*   
+//****On the emulator this code is causing force close due to not being able to 
+//    get GPS or network location, commented out for now 
+
 //----------------------------------get location--------------------
-        
+       
+      
         LocationResult locationResult = new LocationResult(){
             @Override
             public void gotLocation(Location location){
                 //Got the location!
             	// now use location.methods to get long and lat to pass to http request
+            	JSONWorker jaSON = new JSONWorker();
+            	jaSON.getUrlMyLocation(location.getLongitude()+"", location.getLongitude()+"");
             }
         };
         GetLocation myLocation = new GetLocation();
         myLocation.getLocation(this, locationResult);        
         
 //----------------------------------end get location--------------------        
-        
+ */       
 
         
         //Open the search dialogue on app-launch
+        
         onSearchRequested();
+        
+        //call JSON worker to parse JSON String
+        JSONWorker worker = new JSONWorker();
     }
     
     
@@ -57,8 +70,7 @@ public class CarJuiceActivity extends Activity {
     	
     	//Starts instance of the activity called by intent parameter, in this case: DisplayMessageActivity
     	startActivity(intent);
+    	//TestJSON test = new TestJSON();
     }
-    
-   
-    
+
 }
